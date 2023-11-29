9ac2d3e8b0123d0e88934a6ed88bc876573065d1
Akash Pulluru <akashpulluru@gmail.com>
2023-11-29 15:31:15 -0500

    initial d3 in

--- frontend/src/components/Runs/RunGraph.js
+++ frontend/src/components/Runs/RunGraph.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
+import React, { useEffect, useRef } from 'react';
 
 function RunGraph({ runningData }) {
   const svgRef = useRef();
 
   useEffect(() => {
+    if (!runningData.length) return;
+
+    // Assume runningData is an array of objects { date: 'YYYY-MM-DD', value: number }
     const margin = { top: 20, right: 20, bottom: 30, left: 50 };
     const width = 600 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
@@ -13,33 +16,41 @@ function RunGraph({ runningData }) {
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    const xScale = d3.scaleLinear()
-      .domain([0, runningData.length])
+    // Create scales
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(runningData, d => new Date(d.date)))
       .range([0, width]);
 
     const yScale = d3.scaleLinear()
-      .domain([0, d3.max(runningData, d => d.time)])
+      .domain([0, d3.max(runningData, d => d.value)])
       .range([height, 0]);
 
+    // Create axes
+    const xAxis = d3.axisBottom(xScale).ticks(5);
+    const yAxis = d3.axisLeft(yScale);
+
+    // Append axes to the svg
+    svg.append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(xAxis);
+
+    svg.append('g')
+      .call(yAxis);
+
+    // Draw line
     const line = d3.line()
-      .x((d, i) => xScale(i))
-      .y(d => yScale(d.time));
+      .x(d => xScale(new Date(d.date)))
+      .y(d => yScale(d.value))
+      .curve(d3.curveMonotoneX);
 
     svg.append('path')
       .datum(runningData)
-      .attr('class', 'line')
-      .attr('d', line)
       .attr('fill', 'none')
-      .attr('stroke', 'steelblue');
-
-    svg.append('g')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(xScale));
-
-    svg.append('g')
-      .call(d3.axisLeft(yScale));
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', 2)
+      .attr('d', line);
   }, [runningData]);
 
   return <svg ref={svgRef} />;
--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,32 +1,47 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import RunGraph from '../Runs/RunGraph';
+import { fetchUserRuns } from '../../store/runs';
 
 const UserProfile = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.session.user);
-  const loggedIn = useSelector(state => !!state.session.user);
+  const runs = useSelector(state => state.runs.user || []); 
 
-  if (!user) {
-    return null;
-  }
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchUserRuns(user._id));
+    }
+  }, [dispatch, user]);
 
-  if (loggedIn) {
-    return (
-        <main>
+  // Check if runs is an array before trying to map over it
+  const transformedRunsData = Array.isArray(runs) ? runs.map(run => ({
+    date: new Date(run.createdAt),
+    value: run.distance
+  })) : [];
 
-            <h1>User Profile</h1>
-            <div>Name : {user.username}</div>
-            <div>Email : {user.email}</div>
-            <div>Gender :</div>
-            <div>Age :</div>
-            <div>Location :</div>
-            <div>Height :</div>
-            <div>Weight :</div>
+  // Transform runs data to match RunGraph format if necessary
 
-        </main>
-    )
-  }
+  return (
+    <main>
+      {/* ...other user profile details... */}
+      <h1>User Profile</h1>
+      <div>Name : {user.username}</div>
+      <div>Email : {user.email}</div>
+      <div>Gender :</div>
+      <div>Age :</div>
+      <div>Location :</div>
+      <div>Height :</div>
+      <div>Weight :</div>
+      <div>
+        <h3>Your Running Data</h3>
+        <RunGraph runningData={transformedRunsData} />
+      </div>
+    </main>
+  );
 };
 
 export default UserProfile;
+
+ 
